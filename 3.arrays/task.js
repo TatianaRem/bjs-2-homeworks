db9cea3cab58b5ba68317f83f60210cc441a0f93
Tatiana Rem <tatiana13d@yandex.ru>
2023-06-15 18:20:53 +0600

    Functions were created to compare and filter arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,12 @@
+//Сравнение значений элементов двух массивов
 function compareArrays(arr1, arr2) {
-  
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    return arr1.every((item, index) => item === arr2[index]);
 }
 
+//Расчет среднего значения возраста пользователей одного пола
 function getUsersNamesInAgeRange(users, gender) {
-  
+	return result = users.filter((user) => user.gender === gender).map((user) => user.age).reduce((acc, age, index, arr) => acc + age / arr.length, 0);
 }
