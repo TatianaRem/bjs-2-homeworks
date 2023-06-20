936cb49d59f9767a88a50e725bf1d8f913d49b48
Tatiana Rem <tatiana13d@yandex.ru>
2023-06-20 14:27:45 +0600

    Methods were created

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+//Добавление оценок, проверка сущестования свойств marks
+Student.prototype.addMarks = function(...marks) {
+	if (this.hasOwnProperty('marks')) {
+		this.marks.push(...marks)
+	}
+
 }
 
-Student.prototype.getAverage = function () {
-  
+//Расчет среднего арифмитического оценок студента, проверка наличия оценок у студента
+Student.prototype.getAverage = function() {
+	if (this.hasOwnProperty('marks') && this.marks.length > 0) {
+		return this.marks.reduce((acc, mark) => acc + mark, 0) / this.marks.length;
+	}
+	return 0;
+
 }
 
-Student.prototype.exclude = function (reason) {
-  
+//Исключение студента и установление причины исключения
+Student.prototype.exclude = function(reason) {
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
+
 }
